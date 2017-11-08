803bb3765f3230be1d9e612e6fa0e31c2854f745
Alexandre Langlais <alexandre.langlais@edoc.fr>
2017-11-08 15:52:55 +0100

    first commit

---
+++ src/main/scala/List.scala
@@ -0,0 +1,136 @@
+import scala.annotation.tailrec
+
+sealed trait MyList[+A]
+case object MyNil extends MyList[Nothing]
+case class Cons[+A](h: A, tail: MyList[A]) extends MyList[A]
+
+object MyList {
+  def sum(ints: MyList[Int]): Int = ints match {
+    case MyNil       => 0
+    case Cons(x, xs) => x + sum(xs)
+  }
+
+  def product(ints: MyList[Double]): Double = ints match {
+    case MyNil        => 1.0
+    case Cons(0.0, _) => 0.0
+    case Cons(x, xs)  => x * product(xs)
+  }
+
+  def foldRight[A, B](list: MyList[A], z: B)(f: (A, B) => B): B = list match {
+    case MyNil       => z
+    case Cons(x, xs) => f(x, foldRight(xs, z)(f))
+  }
+
+  @tailrec
+  def foldLeft[A, B](list: MyList[A], z: B)(f: (B, A) => B): B = list match {
+    case MyNil       => z
+    case Cons(x, xs) => foldLeft(xs, f(z, x))(f)
+  }
+
+  def apply[A](elems: A*): MyList[A] = {
+    if (elems.isEmpty) MyNil
+    else Cons[A](elems.head, apply(elems.tail: _*))
+  }
+
+  def tail[A](list: MyList[A]): MyList[A] = list match {
+    case Cons(_, xs) => xs
+    case MyNil       => MyNil
+  }
+
+  def setHead[A](list: MyList[A], newHead: A): MyList[A] = Cons(newHead, list)
+
+  @tailrec
+  def drop[A](list: MyList[A], n: Int): MyList[A] = {
+    if (n == 0) list
+    else
+      list match {
+        case MyNil      => MyNil
+        case Cons(x, _) => drop(tail(list), n - 1)
+      }
+  }
+
+  def dropWhile[A](list: MyList[A])(p: A => Boolean): MyList[A] = list match {
+    case MyNil      => MyNil
+    case Cons(x, _) => if (p(x)) dropWhile(tail(list))(p) else list
+  }
+
+  def init[A](l: MyList[A]): MyList[A] = l match {
+    case MyNil          => MyNil
+    case Cons(h, MyNil) => MyNil
+    case Cons(h, xs)    => Cons(h, init(xs))
+  }
+
+  def length[A](as: MyList[A]): Int = {
+    foldRight(as, 0)((_, acc) => acc + 1)
+  }
+
+  def sumL(l: MyList[Int]): Int = {
+    foldLeft(l, 0)(_ + _)
+  }
+
+  def productL(l: MyList[Double]): Double = {
+    foldLeft(l, 1.0)(_ * _)
+  }
+
+  def lengthL[A](l: MyList[A]): Int = {
+    foldLeft(l, 0)((acc, _) => acc + 1)
+  }
+
+  def reverse[A](l: MyList[A]): MyList[A] = {
+    foldLeft(l, MyNil: MyList[A])((b, a) => Cons(a, b))
+  }
+
+  def foldRightUsingFoldLeft[A, B](l: MyList[A], z: B)(f: (A, B) => B): B = {
+    foldLeft(l, z)((b, a) => f(a, b))
+  }
+
+  def append[A](as: MyList[A], e: A): MyList[A] = {
+    foldLeft(reverse(as), Cons(e, MyNil))((b, a) => Cons(a, b))
+  }
+
+  def appendList[A](l: MyList[A], r: MyList[A]): MyList[A] =
+    foldRight(l, r)(Cons(_,_))
+
+  def concat[A](as: MyList[MyList[A]]): MyList[A] = {
+    foldRight(as, MyNil: MyList[A])(appendList)
+  }
+
+  def addOne(as: MyList[Int]): MyList[Int] = {
+    foldLeft(reverse(as), MyNil:MyList[Int])((b, a) => Cons(a + 1, b))
+  }
+
+  def toString(as: MyList[Double]): String = {
+    foldLeft(as, "")((acc, a) => acc + a.toInt.toString)
+  }
+
+  def map[A, B](as: MyList[A])(f: A => B): MyList[B] = {
+    foldRight(as, MyNil:MyList[B])((a, b) => Cons(f(a), b))
+  }
+
+  def filter[A](as: MyList[A])(p: A => Boolean): MyList[A] = {
+    foldRight(as, MyNil:MyList[A])((a, b) => if (p(a)) Cons(a, b) else b)
+  }
+
+  def flatMap[A, B](as: MyList[A])(f: A => MyList[B]): MyList[B] = {
+    foldRight(as, MyNil:MyList[B])((a, b) => appendList(f(a), b))
+  }
+
+  def filterF[A](as: MyList[A])(p: A => Boolean): MyList[A] = {
+    flatMap(as)(x => if(p(x)) MyList(x) else MyNil)
+  }
+}
+
+object MyApp extends App {
+  val x = MyList(1, 2, 3, 4, 5) match {
+    case Cons(x, Cons(2, Cons(4, _)))          => x
+    case MyNil                                 => 42
+    case Cons(x, Cons(y, Cons(3, Cons(4, _)))) => x + y
+    case Cons(h, t)                            => h + MyList.sum(t)
+    case _                                     => 101
+  }
+  println(x)
+
+  val value: Int = 0
+  val l = MyList.foldRight(MyList(1, 2, 3), MyNil: MyList[Int])(Cons(_, _))
+  println(l)
+}
---
+++ src/test/scala/ExampleSpec.scala
@@ -0,0 +1,231 @@
+import collection.mutable.Stack
+import org.scalatest._
+
+class ExampleSpec extends FlatSpec with Matchers {
+
+  "MyList" should "be able to compute sum of ints" in {
+    val list: MyList[Int] = MyList(1, 2, 3)
+
+    MyList.sum(list) shouldBe (6)
+  }
+
+  it should "be able to multiply every items in the list" in {
+    val list: MyList[Double] = MyList(1, 2, 3)
+
+    MyList.product(list) shouldBe (6.0)
+  }
+
+  it should "return 1 if Nil" in {
+    val list: MyList[Double] = MyNil
+
+    MyList.product(list) shouldBe (1.0)
+  }
+
+  it should "return 0 if one element is 0" in {
+    val list: MyList[Double] = MyList(1, 2, 0, 4)
+
+    MyList.product(list) shouldBe (0)
+  }
+
+  "Tail function" should "return last elements from normal list" in {
+    val list: MyList[Int] = MyList(1, 2, 3)
+
+    MyList.tail(list) shouldBe (MyList(2, 3))
+  }
+
+  it should "return nil on a Nil list" in {
+    val list: MyList[Int] = MyNil
+
+    MyList.tail(list) shouldBe (MyNil)
+  }
+
+  it should "return Nil on a 1 element list" in {
+    val list: MyList[Int] = MyList(1)
+
+    MyList.tail(list) shouldBe (MyNil)
+  }
+
+  "Sethead function" should "add an element in front of a list" in {
+    val list: MyList[Int] = MyList(2, 3)
+
+    MyList.setHead(list, 1) shouldBe (MyList(1, 2, 3))
+
+  }
+
+  it should "return a one element list if passed argument is nil" in {
+    MyList.setHead(MyNil, 1) shouldBe (MyList(1))
+  }
+  it should "add an element in front of a one item list" in {
+    MyList.setHead(MyList(2), 1) shouldBe (MyList(1, 2))
+  }
+
+  "Drop function" should "return last elements from normal list" in {
+    val list: MyList[Int] = MyList(1, 2, 3, 4, 5)
+
+    MyList.drop(list, 2) shouldBe (MyList(3, 4, 5))
+  }
+
+  it should "return nil on a Nil list" in {
+    val list: MyList[Int] = MyNil
+
+    MyList.drop(list, 2) shouldBe (MyNil)
+  }
+
+  it should "return nil on a n > list size" in {
+    val list: MyList[Int] = MyList(1, 2)
+
+    MyList.drop(list, 3) shouldBe (MyNil)
+  }
+
+  it should "return nil on a n == list size" in {
+    val list: MyList[Int] = MyList(1, 2)
+
+    MyList.drop(list, 2) shouldBe (MyNil)
+  }
+
+  "Drop while function" should "drop first even element of a list" in {
+    val list: MyList[Int] = MyList(2, 4, 6, 7, 8)
+
+    MyList.dropWhile(list)(x => x % 2 == 0) shouldBe (MyList(7, 8))
+  }
+
+  it should "return nil on a nil list" in {
+    MyList.dropWhile(MyNil)((x:Int) => x % 2 == 0) shouldBe (MyNil)
+  }
+
+  it should "return nil on a all predicate found" in {
+    MyList.dropWhile(MyList(1, 3, 5, 7))(x => x % 2 != 0) shouldBe (MyNil)
+  }
+
+  it should "return same list on a no predicate found" in {
+    MyList.dropWhile(MyList(1, 3, 5, 7))(x => x % 2 == 0) shouldBe (MyList(1, 3, 5, 7))
+  }
+
+  "Init function" should "return all but last elements of a list" in {
+    val list: MyList[Int] = MyList(2, 4, 6, 7, 8)
+
+    MyList.init(list) shouldBe (MyList(2, 4, 6, 7))
+  }
+
+  it should "return nil on a one element list" in {
+    MyList.init(MyList(1)) shouldBe (MyNil)
+
+  }
+  it should "return nil on a nil list" in {
+    MyList.init(MyNil) shouldBe (MyNil)
+
+  }
+  it should "return first element on a two elements list" in {
+    MyList.init(MyList(1, 2)) shouldBe (MyList(1))
+  }
+
+  "Fold right" should "apply add operation on every element of a list" in {
+    val list: MyList[Int] = MyList(1, 2, 3, 4)
+
+    MyList.foldRight(list, 0)(_ + _) shouldBe (10)
+
+  }
+  it should "apply multiply operation on every element of a list" in {
+    val list: MyList[Int] = MyList(1, 2, 3, 4)
+
+    MyList.foldRight(list, 1)(_ * _) shouldBe (24)
+
+  }
+
+  "Length using fold right" should "return the correct length of a list" in {
+    MyList.length(MyList(1, 2, 3, 4, 5)) shouldBe (5)
+    MyList.length(MyList(1, 2)) shouldBe (2)
+    MyList.length(MyList(1)) shouldBe (1)
+  }
+
+  it should "return 0 on a nil list" in {
+    MyList.length(MyNil) shouldBe (0)
+  }
+
+  "Fold left" should "apply add operation on every element of a list" in {
+    val list: MyList[Int] = MyList(1, 2, 3, 4)
+
+    MyList.foldLeft(list, 0)(_ + _) shouldBe (10)
+
+  }
+  it should "apply multiply operation on every element of a list" in {
+    val list: MyList[Int] = MyList(1, 2, 3, 4)
+
+    MyList.foldLeft(list, 1)(_ * _) shouldBe (24)
+
+  }
+
+  "Length using fold left" should "return the correct length of a list" in {
+    MyList.lengthL(MyList(1, 2, 3, 4, 5)) shouldBe (5)
+    MyList.lengthL(MyList(1, 2)) shouldBe (2)
+    MyList.lengthL(MyList(1)) shouldBe (1)
+  }
+
+  it should "return 0 on a nil list" in {
+    MyList.lengthL(MyNil) shouldBe (0)
+  }
+
+  "Sum using fold left" should "return the sum of the elements" in {
+    MyList.sumL(MyList(1, 2, 3, 4, 5)) shouldBe (15)
+  }
+
+  "Product using fold left" should "return the product of the elements" in {
+    MyList.productL(MyList(1, 2, 3, 4, 5)) shouldBe (120)
+  }
+
+  "Reverse using a fold" should "return the reversed list" in {
+    MyList.reverse(MyList(1, 2, 3)) shouldBe (MyList(3, 2, 1))
+    MyList.reverse(MyList(1, 2)) shouldBe (MyList(2, 1))
+    MyList.reverse(MyList(1)) shouldBe (MyList(1))
+    MyList.reverse(MyNil) shouldBe (MyNil)
+  }
+
+  "Fold right using fold left" should "be the same as foldright" in {
+    val list: MyList[Int] = MyList(1, 2, 3, 4)
+    MyList.foldRight(list, 0)(_ + _) shouldBe (MyList.foldRightUsingFoldLeft(list, 0)(_ + _))
+  }
+
+  "Append" should "append an element to a list" in {
+    MyList.append(MyList(1, 2, 3, 4), 5) shouldBe (MyList(1, 2, 3, 4, 5))
+    MyList.append(MyList(1), 2) shouldBe (MyList(1, 2))
+    MyList.append(MyNil, 5) shouldBe (MyList(5))
+  }
+
+  "Concat" should "concatenate a list of lists into a single list" in {
+    val l1 = MyList(1, 2, 3)
+    val l2 = MyList(4, 5, 6)
+    val l3 = MyList(7, 8, 9)
+    val con = MyList(l1, l2, l3)
+
+    val res = MyList.concat(con)
+
+    res shouldBe (MyList(1, 2, 3, 4, 5, 6, 7, 8, 9))
+  }
+
+  "AddOne" should "add one to every element of the list" in {
+    MyList.addOne(MyList(1, 2, 3)) shouldBe (MyList(2, 3, 4))
+  }
+
+  "ToString" should "return a string from a list of doubles" in {
+    MyList.toString(MyList(1, 2, 3)) shouldBe ("123")
+  }
+
+  "Map" should "modify a list maintaining its structure" in {
+    MyList.map(MyList(1, 2, 3))(_ * 2) shouldBe (MyList(2, 4, 6))
+  }
+
+  "Filter" should "filter elements of a list" in {
+    MyList.filter(MyList(1, 2, 3, 4, 5, 6))(_ % 2 == 0) shouldBe (MyList(2, 4, 6))
+    MyList.filter(MyList(1, 2, 3, 4, 5, 6))(_ < 4) shouldBe (MyList(1, 2, 3))
+  }
+  "Flatmap" should "Return a flat list" in {
+    MyList.flatMap(MyList(1, 2, 3))(i => MyList(i, i)) shouldBe (MyList(1, 1, 2, 2, 3, 3))
+  }
+
+  "FilterF" should "filter elements of a list" in {
+    MyList.filterF(MyList(1, 2, 3, 4, 5, 6))(_ % 2 == 0) shouldBe (MyList(2, 4, 6))
+    MyList.filterF(MyList(1, 2, 3, 4, 5, 6))(_ < 4) shouldBe (MyList(1, 2, 3))
+  }
+
+
+}
