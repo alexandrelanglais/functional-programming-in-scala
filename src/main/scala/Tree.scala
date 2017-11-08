3d2897d795a5676c056c60fe64062bf1f6408354
Alexandre Langlais <alexandre.langlais@edoc.fr>
2017-11-08 18:02:41 +0100

    Implemented fold for trees

--- src/main/scala/Tree.scala
+++ src/main/scala/Tree.scala
@@ -3,35 +3,53 @@ case class Leaf[A](value: A) extends Tree[A]
 case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 
 object Tree {
-  def size[A](tree: Tree[A]): Int = {
-    def go(t: Tree[A], n: Int): Int = t match {
-      case Leaf(_)      => n + 1
-      case Branch(l, r) => go(l, n) + go(r, n) + 1
-    }
-    go(tree, 0)
+  def size_1[A](t: Tree[A]): Int = t match {
+    case Leaf(_)      => 1
+    case Branch(l, r) => size(l) + size(r) + 1
   }
 
-  def maximum(tree: Tree[Int]): Int = {
-    def go(t: Tree[Int], m: Int): Int = t match {
-      case Leaf(x)      => x.max(m)
-      case Branch(l, r) => go(l, go(r, m))
-    }
-    go(tree, 0)
+  def maximum_1(t: Tree[Int]): Int = t match {
+    case Leaf(x)      => x
+    case Branch(l, r) => maximum(l) max maximum(r)
   }
 
-  def depth[A](tree: Tree[A]): Int = {
-    def go(t: Tree[A], n: Int, m: Int): Int = t match {
-      case Leaf(_)      => n.max(m)
-      case Branch(l, r) => go(r, go(l, (n + 1), (n + 1).max(m)), (n + 1).max(m))
-    }
-    go(tree, 0, 0)
+  def depth_1[A](t: Tree[A]): Int = t match {
+    case Leaf(_)      => 0
+    case Branch(l, r) => 1 + (depth(r) max depth(l))
   }
 
-  def map[A, B](tree: Tree[A])(f: A => B): Tree[B] = {
-    def go(t: Tree[A]): Tree[B] = t match {
-      case Leaf(x) => Leaf(f(x))
-      case Branch(l, r) => Branch(go(l), go(r))
-    }
-    go(tree)
+  def map_1[A, B](t: Tree[A])(f: A => B): Tree[B] = t match {
+    case Leaf(x)      => Leaf(f(x))
+    case Branch(l, r) => Branch(map(l)(f), map(r)(f))
   }
+
+  def size[A](t: Tree[A]): Int = {
+    fold(t, (x: A) => 1) { (l, r) =>
+      1 + size(l) + size(r)
+    }
+  }
+
+  def maximum(t: Tree[Int]): Int = {
+    fold(t, (x: Int) => x) { (l, r) =>
+      maximum(l) max maximum(r)
+    }
+  }
+
+  def depth[A](t: Tree[A]): Int = {
+    fold(t, (x: A) => 0) { (l, r) =>
+      1 + (depth(r) max depth(l))
+    }
+  }
+
+  def map[A, B](t: Tree[A])(f: A => B): Tree[B] = {
+    fold(t, (x:A) => Leaf(f(x)): Tree[B]) { (l, r) =>
+      Branch(map(l)(f), map(r)(f))
+    }
+  }
+
+  def fold[A, B](t: Tree[A], z: (A => B))(f: (Tree[A], Tree[A]) => B): B =
+    t match {
+      case Leaf(x)      => z(x)
+      case Branch(l, r) => f(l, r)
+    }
 }
