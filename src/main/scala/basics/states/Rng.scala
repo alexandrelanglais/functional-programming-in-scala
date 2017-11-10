2cb2f7eb67c8d9559a8d5f66f4c5155142a62b04
Alexandre Langlais <alexandre.langlais@edoc.fr>
2017-11-10 15:09:44 +0100

    Started state course

---
+++ src/main/scala/basics/states/Rng.scala
@@ -0,0 +1,88 @@
+package basics.states
+
+import scala.annotation.tailrec
+
+trait RNG {
+
+  type Rand[+A] = RNG => (A, RNG)
+
+  def nextInt: (Int, RNG)
+
+  val int: Rand[Int] = _.nextInt
+
+  def randomPair: Rand[(Int, Int)] = rng => {
+    val (i1, rng2) = rng.nextInt
+    val (i2, rng3) = rng.nextInt
+    ((i1, i2), rng3)
+  }
+
+  def nonNegativeInt: Rand[Int] = rng => {
+    val (n, r) = rng.nextInt
+    val p =
+      if (n == Int.MinValue) math.abs(n + 1)
+      else math.abs(n)
+    (p, r)
+  }
+
+  def double: Rand[Double] = rng => {
+    val (ri, rng2) = nonNegativeInt(rng)
+    val d = ri.toDouble / Int.MaxValue
+    (d, rng2)
+  }
+
+  def intDouble: Rand[(Int, Double)] = rng => {
+    val (i, r) = rng.nextInt
+    val (d, r2) = double(r)
+    ((i, d), r2)
+  }
+
+  def doubleInt: Rand[(Double, Int)] = rng => {
+    val ((i, d), r) = intDouble(rng)
+    ((d, i), r)
+  }
+
+  def double3: Rand[(Double, Double, Double)] = rng => {
+    val (d, r) = double(rng)
+    val (d2, r2) = double(r)
+    val (d3, r3) = double(r2)
+
+    ((d, d2, d3), r3)
+  }
+
+  def ints(count: Int): Rand[List[Int]] = rng => {
+    def go(n: Int, l: List[Int]): Rand[List[Int]] = r => {
+      if (n < count) {
+        val (i, r2) = r.nextInt
+        go(n + 1, i :: l)(r2)
+      }
+      else (l, r)
+    }
+
+    go(0, Nil)(rng)
+  }
+
+  def unit[A](a: A): Rand[A] =
+    rng => (a, rng)
+
+  def map[A, B](s: Rand[A])(f: A => B): Rand[B] =
+    rng => {
+      val (a, rng2) = s(rng)
+      (f(a), rng2)
+    }
+
+  def nonNegativeEven: Rand[Int] =
+    map(nonNegativeInt)(i => i - i % 2)
+
+  def elegantDouble: Rand[Double] =
+    map(_.nextInt)(i => i.toDouble / Int.MaxValue)
+}
+
+case class SimpleRNG(seed: Long) extends RNG {
+  override def nextInt: (Int, RNG) = {
+    val newSeed = (seed * 0x5DEECE66DL + 0xBL) & 0xFFFFFFFFFFFFL
+    val nextRNG = SimpleRNG(newSeed)
+    val n = (newSeed >>> 16).toInt
+    (n, nextRNG)
+  }
+}
+
---
+++ src/test/scala/basics/states/RngTest.scala
@@ -0,0 +1,57 @@
+package basics.states
+
+import org.scalatest._
+
+class RngTest extends FlatSpec with Matchers {
+
+  val rng = new SimpleRNG(515L)
+
+  "nonNegativeValue" should "return a positive value" in {
+    assert(rng.nonNegativeInt(rng)._1 > 0)
+  }
+
+  "double" should "return a value between 0 and 1" in {
+    val (n, _) = rng.double(rng)
+    assert(0 < n && n < 1)
+  }
+
+  "intDouble" should "generate a random pair of a double and a int" in {
+    val ((i, d), r) = rng.intDouble(rng)
+
+    assert(0 < d && d < 1)
+    assert(i != d)
+    assert(r.nextInt != i)
+  }
+
+  "doubleInt" should "generate a random pair of a double and a int" in {
+    val ((d, i), r) = rng.doubleInt(rng)
+
+    assert(0 < d && d < 1)
+    assert(i != d)
+    assert(r.nextInt != i)
+  }
+
+  "double3" should "generate a triplet of doubles" in {
+    val ((d, d2, d3), r) = rng.double3(rng)
+
+    assert(0 < d && d < 1)
+    assert(0 < d2 && d2 < 1)
+    assert(0 < d3 && d3 < 1)
+    assert(d != d2 && d2 != d3)
+  }
+
+  "ints" should "generate a list of random numbers" in {
+    val (l, r) = rng.ints(5)(rng)
+
+    println(l)
+    assert(l.size == 5)
+    assert(l.toSet.size == 5)
+  }
+
+  "elegantDouble" should "return a value between 0 and 1" in {
+    val (n, r) = rng.elegantDouble(rng)
+
+    assert(0 < n && n < 1)
+  }
+
+}
